f73e9fac8105fae858fa686d8e2c15d5d5b9fa28
Dimitri Bouniol <dimitribouniol@mochidev.com>
2024-01-11 02:22:43 -0800

    Clarified documentation for validation and fixed typos/inconsistencies

--- Sources/AcmeSwift/Models/Account/AcmeAccountInfo.swift
+++ Sources/AcmeSwift/Models/Account/AcmeAccountInfo.swift
@@ -1,32 +1,33 @@
 import Foundation
 
-/// Account information returned when calling `get()` or `create()`
+/// Account information returned when calling `get()` or `create()`.
 public struct AcmeAccountInfo: Codable {
     
-    /// URL containing the ID of the Account
-    /// This URL is used to performa some account management operations.
+    /// URL containing the ID of the Account.
+    ///
+    /// - Note: This URL is used to perform some account management operations.
     internal(set) public var url: URL?
     
-    /// Information about the Account public key in JWK format
+    /// Information about the Account public key in JWK format.
     public let key: JWK
     
-    /// The PEM representation of the private key for this Account
+    /// The PEM representation of the private key for this Account.
     internal(set) public var privateKeyPem: String?
     
-    /// The contact entries
+    /// The contact entries.
     public let contact: [String]
     
-    /// Source IP (as seen by the ACME servers) from which the Account was created
+    /// Source IP (as seen by the ACME servers) from which the Account was created.
     public let initialIp: String
     
-    /// Date when the Account was created
+    /// Date when the Account was created.
     public let createdAt: String
     
-    /// Current status of the Account
+    /// Current status of the Account.
     public let status: Status
     
-    /// URL to the pending orders for this account
-    /// No provider seems to have this fully implemented
+    /// URL to the pending orders for this account.
+    /// - Note: No provider seems to have this fully implemented.
     public let orders: URL?
     
     public enum Status: String, Codable {
