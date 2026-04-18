4d681739addafa28cf4b171ca9811370d4179026
Matthieu Barthelemy <bonsouere@gmail.com>
2026-04-18 08:21:24 +0200

    Migrate tests to Swift Testing

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
             name: "AcmeSwift",
             dependencies: [
                 .product(name: "AsyncHTTPClient", package: "async-http-client"),
-                .product(name: "NIOFoundationCompat", package: "swift/nio"),
+                .product(name: "NIOFoundationCompat", package: "swift-nio"),
                 .product(name: "Crypto", package: "swift-crypto"),
                 .product(name: "_CryptoExtras", package: "swift-crypto"),
                 .product(name: "JWTKit", package: "jwt-kit"),
